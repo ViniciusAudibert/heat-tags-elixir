3ed1fa04119d3a092e1d03852b673a4050c15b4b
Vinicius Audibert <vini.audibert@gmail.com>
2021-10-21 23:43:16 -0300

    feat: adicionar insert message

---
+++ lib/heat_tags/message.ex
@@ -0,0 +1,23 @@
+defmodule HeatTags.Message do
+  use Ecto.Schema
+
+  import Ecto.Changeset
+
+  @required_params [:message, :username, :email]
+
+  schema "messages" do
+    field :message, :string
+    field :username, :string
+    field :email, :string
+
+    timestamps()
+  end
+
+  def changeset(params) do
+    %__MODULE__{}
+    |> cast(params, @required_params)
+    |> validate_required(@required_params)
+    |> validate_length(:message, min: 1, max: 123)
+    |> validate_format(:email, ~r/@/)
+  end
+end
